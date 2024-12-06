21d0808cd65af04bea043f9471bfe19cb56ec389
dfrrrrse <zxzx37755@gmail.com>
2024-12-06 20:57:06 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,48 @@
+/*
+                                                                            _   _                           _   _           _  
+                                                                            | \ | |                         | | | |         | | 
+                                                                            |  \| | ___  ___ _ __ ___  _ __ | |_| | ___  ___| |_
+                                                                            | . ` |/ _ \/ __| '__/ _ \| '_ \|  _  |/ _ \/ __| __|
+                                                                            | |\  |  __/ (__| | | (_) | | | | | | | (_) \__ \ |_
+                                                                            \_| \_/\___|\___|_|  \___/|_| |_\_| |_/\___/|___/\__|
+                                                                                                                                
+                                                                            This bot is made by necron.host, discord.gg/nec
+                                                                            ⚠ All rights reserved to the team NecronHost ⚠
+
+*/
+
+
+
+module.exports = {
+    token: '',//توكن البوت
+    prefix: '!',//بريفكس البوت
+    welcomechannel: '1307567269857067112',//ايدي روم الترحيب
+    welcomemsg: '{user} has invited {inviter} to {server}',
+    //{user} = user mention
+    //{inviter} = inviter
+    //{server} = server name
+    //example: {user} has invited {inviter} to {server}
+    welcomeimage: 'https://cdn.discordapp.com/attachments/1295431835211534460/1314650778375426118/IMG_6731.jpg?ex=67548b72&is=675339f2&hm=f24a9b91771221774bf922a1043273cebe47459f2f6d3a4b484a0b699eb9967c&',//رابط صورة الترحيب
+    count: 10,//رقم التحكم بالقياسات
+}
+/*
+اوامر البوت:
+!setwelcome | لتعين احداثيات صورة الترحيب
+!settext | لتعين احداثيات يوزر العضو
+*/
+
+
+
+
+/*
+                                                                            _   _                           _   _           _  
+                                                                            | \ | |                         | | | |         | | 
+                                                                            |  \| | ___  ___ _ __ ___  _ __ | |_| | ___  ___| |_
+                                                                            | . ` |/ _ \/ __| '__/ _ \| '_ \|  _  |/ _ \/ __| __|
+                                                                            | |\  |  __/ (__| | | (_) | | | | | | | (_) \__ \ |_
+                                                                            \_| \_/\___|\___|_|  \___/|_| |_\_| |_/\___/|___/\__|
+                                                                                                                                
+                                                                            This bot is made by necron.host, discord.gg/nec
+                                                                            ⚠ All rights reserved to the team NecronHost ⚠
+
+*/
